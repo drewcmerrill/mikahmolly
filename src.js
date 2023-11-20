9cd466f9cbe3cbf418282887bf1ecabb6799ad29
Drew Merrill <drewcmerrill@gmail.com>
2023-11-20 02:20:53 -0700

    update

--- src.js
+++ src.js
@@ -9,83 +9,64 @@ function onScroll() {
   );
 
   // Gradually change the background color based on scroll position
-  stripeContainer.style.height = `${scrollPercentage * 100 + 10}%`;
+  stripeContainer.style.height = `${scrollPercentage * 100}%`;
+}
+
+function drawFilledPolygon(ctx, vertices) {
+  if (vertices.length < 3) {
+    // Need at least 3 vertices to form a polygon
+    console.error("At least 3 vertices are required.");
+    return;
+  }
+
+  ctx.beginPath();
+  ctx.moveTo(vertices[0].x, vertices[0].y);
+
+  for (var i = 1; i < vertices.length; i++) {
+    ctx.lineTo(vertices[i].x, vertices[i].y);
+  }
+
+  // Close the path to complete the polygon
+  ctx.closePath();
+
+  // Fill the polygon
+  ctx.fill();
 }
 
 function drawStreaks() {
-  let studContainer = document.querySelector(".stud-container");
-  let stripeContainer = document.querySelector(".stripe-container");
   let canvas = document.querySelector(".zigzag");
   let ctx = canvas.getContext("2d");
   let dpr = window.devicePixelRatio || 1;
 
-  canvas.width = studContainer.clientWidth;
-  canvas.height = studContainer.clientHeight;
+  canvas.width = canvas.clientWidth * dpr;
+  canvas.height = canvas.clientHeight * dpr;
 
   let zigs = 4;
-  ctx.lineWidth = 40;
+  ctx.fillStyle = "#a5180e";
+  let streakWidth = 200;
 
-  let angle = Math.atan((canvas.clientWidth * zigs) / canvas.clientHeight);
-  let horizontalWidth = ctx.lineWidth / Math.cos(angle) + 5;
-  let verticalHeight = Math.tan(Math.PI / 2 - angle) * (horizontalWidth / 2);
-  console.log(verticalHeight);
-
-  ctx.strokeStyle = "#191dba";
-
-  ctx.beginPath();
-  ctx.moveTo(0, -verticalHeight);
-  for (let i = 1; i <= zigs; i++) {
-    if (i % 2 == 1) {
-      ctx.lineTo(
-        canvas.width - horizontalWidth * 2,
-        (i / zigs) * canvas.height
-      );
+  let vertices = [];
+  for (let i = 0; i <= zigs; i++) {
+    if (i % 2 === 0) {
+      vertices[i] = { x: 0, y: (i / zigs) * canvas.height };
+      vertices[zigs * 2 + 1 - i] = {
+        x: streakWidth,
+        y: (i / zigs) * canvas.height,
+      };
     } else {
-      ctx.lineTo(horizontalWidth / 2, (i / zigs) * canvas.height);
-    }
-  }
-  ctx.stroke();
-
-  ctx.strokeStyle = "#a5170d";
-
-  ctx.beginPath();
-  ctx.moveTo(horizontalWidth, -verticalHeight);
-  for (let i = 1; i <= zigs; i++) {
-    if (i % 2 == 1) {
-      if (i === zigs) {
-        ctx.lineTo(canvas.width, (i / zigs) * canvas.height + verticalHeight);
-      } else {
-        ctx.lineTo(
-          canvas.width - horizontalWidth / 2,
-          (i / zigs) * canvas.height
-        );
-      }
-    } else {
-      if (i === zigs) {
-        ctx.lineTo(0, (i / zigs) * canvas.height + verticalHeight);
-      } else {
-        ctx.lineTo(horizontalWidth / 2, (i / zigs) * canvas.height);
-      }
+      vertices[i] = {
+        x: canvas.width - streakWidth,
+        y: (i / zigs) * canvas.height,
+      };
+      vertices[zigs * 2 + 1 - i] = {
+        x: canvas.width,
+        y: (i / zigs) * canvas.height,
+      };
     }
   }
 
-  ctx.stroke();
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = 2;
-
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  for (let i = 1; i <= zigs; i++) {
-    if (i % 2 == 1) {
-      ctx.lineTo(canvas.width, (i / zigs) * canvas.height);
-    } else {
-      ctx.lineTo(0, (i / zigs) * canvas.height);
-    }
-  }
-  ctx.stroke();
-
-  stripeContainer.style.height = "10%";
-  // stripeContainer.style.transition = "height 1s ease";
+  // Draw the filled polygon
+  drawFilledPolygon(ctx, vertices);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
