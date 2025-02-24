3f9bab9f993423b21754f068c69dcb2f442c4558
Drew Merrill <drewcmerrill@gmail.com>
2025-02-24 10:19:12 -0700

    Update download filepath

--- app/api/download/route.js
+++ app/api/download/route.js
@@ -2,12 +2,15 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 
 export async function GET() {
-  const filePath = "/root/media/workout.pdf";
+  const filePath = "/root/media/Mikah and Molly 12 Week Training Program.pdf";
 
   try {
     const fileBuffer = fs.readFileSync(filePath);
     const headers = new Headers();
-    headers.append("Content-Disposition", "attachment; filename=workout.pdf");
+    headers.append(
+      "Content-Disposition",
+      "attachment; filename=Mikah and Molly 12 Week Training Program.pdf"
+    );
     headers.append("Content-Type", "application/pdf");
 
     return new NextResponse(fileBuffer, { status: 200, headers });
